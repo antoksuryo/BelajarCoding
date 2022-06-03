7991636aeeaa11500f42e1a2d959468d7de0f8cc
antoksuryo <105631615+antoksuryo@users.noreply.github.com>
2022-06-03 14:19:54 +0700

    Add files via upload

---
+++ refleksi variable modul 3.py
@@ -0,0 +1,43 @@
+x = 7
+print(x)
+
+print(x + 3)
+print(x)
+
+x = 123.456
+print(x)
+
+x = "This is a string"
+print(x + "!")
+
+x = input()
+print("You entered: " + x) 
+
+name = input("Enter your name: ")
+
+print("Hello, " + name) 
+
+age = 42
+print("His age is " + str(age))  
+
+name = input()
+age = input()
+
+print(name + " is " + age)   
+
+x = 8
+
+x -= 2
+print(x)
+
+x /= 3
+print(x)
+
+x **= 5
+print(x)
+
+miles = int(input())
+km = miles * 1.60934
+
+print(km)
+
