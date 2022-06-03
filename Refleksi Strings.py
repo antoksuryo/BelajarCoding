0a87ba1052c3d7262d120612bda79582415de220
antoksuryo <105631615+antoksuryo@users.noreply.github.com>
2022-06-03 11:11:39 +0700

    Add files via upload

---
+++ Refleksi Strings.py
@@ -0,0 +1,12 @@
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+print('One \nTwo \nThree') 
+print("\t hey \t there") 
+print("Hi \r there") 
+print("Newline is \\n") 
+print("""this
+is a
+multiline
+text""") 
+print("spam" * 3)
+
+print(4 * '2')
